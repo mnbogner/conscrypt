b53741451123da0c62ddf9e66a6ed3145447caa3
mnbogner <mnbogner@eds.org>
2025-08-11 14:01:47 -0700

    build fixes

--- common/src/jni/main/cpp/conscrypt/native_crypto.cc
+++ common/src/jni/main/cpp/conscrypt/native_crypto.cc
@@ -7786,7 +7786,11 @@ static int sslSelect(JNIEnv* env, int type, jobject fdObject, AppData* appData,
         if (fds[1].revents & POLLIN) {
             char token;
             do {
-                (void)read(appData->fdsEmergency[0], &token, 1);
+                int foo = 0;
+                foo = read(appData->fdsEmergency[0], &token, 1);
+                if (foo > 0) {
+                    CONSCRYPT_LOG_VERBOSE("FOO: %d", foo);
+                }
             } while (errno == EINTR);
         }
     }
@@ -7817,7 +7821,11 @@ static void sslNotify(AppData* appData) {
     char token = '*';
     do {
         errno = 0;
-        (void)write(appData->fdsEmergency[1], &token, 1);
+        int foo = 0;
+        foo = write(appData->fdsEmergency[1], &token, 1);
+        if (foo > 0) {
+            CONSCRYPT_LOG_VERBOSE("FOO: %d", foo);
+        }
     } while (errno == EINTR);
     errno = errnoBackup;
 #endif
